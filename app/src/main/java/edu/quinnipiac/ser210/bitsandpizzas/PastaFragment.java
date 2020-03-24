b7e673c9962c67e0ed3a34db79603f713f1d9dce
Victoria <toriagorski@gmail.com>
2020-03-23 20:04:34 -0400

    First commit of Bits and Pizzas

---
+++ app/src/main/java/edu/quinnipiac/ser210/bitsandpizzas/PastaFragment.java
@@ -0,0 +1,18 @@
+package edu.quinnipiac.ser210.bitsandpizzas;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import androidx.fragment.app.ListFragment;
+
+public class PastaFragment extends ListFragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflator, ViewGroup container, Bundle savedInstanceState) {
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(inflator.getContext(), android.R.layout.simple_list_item_1, getResources().getStringArray(R.array.pasta));
+        setListAdapter(adapter);
+        return super.onCreateView(inflator, container, savedInstanceState);
+    }
+}
